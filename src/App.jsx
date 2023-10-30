67a2214e2a2c85f98249efe8dc656696ec25cd96
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-10-30 10:42:54 -0300

    Axios React EstudeCe

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import axios from "axios";
+import "./App.css";
 
 function App() {
   const [user, setUser] = useState({});
